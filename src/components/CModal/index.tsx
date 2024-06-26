c869809b6723bde30c72ea07a944a31bcc148a04
Ruveyda-Kışla <ruveydakisla572@gmail.com>
2024-06-26 03:45:54 +0330

    fix: change props names and other minor issues

--- src/components/CModal/index.tsx
+++ src/components/CModal/index.tsx
@@ -9,41 +9,35 @@ import close from 'public/images/close.svg';
 type CModalProps = {
   width?: string;
   title?: string | React.ReactNode;
-  showModal: boolean;
+  isModalOpen: boolean;
   children?: React.ReactNode;
-  handleCloseModal: () => void;
+  onClose: () => void;
 };
 
-const CModal: React.FC<CModalProps> = ({
-  title,
-  children,
-  showModal,
-  width = '482px',
-  handleCloseModal,
-}) => {
+const CModal = ({ title, children, isModalOpen, width = '482px', onClose }: CModalProps) => {
   const modalRef = useRef<HTMLDivElement | null>(null);
   const backdropRef = useRef<HTMLDivElement | null>(null);
 
-  useOutsideClickHandler(showModal, handleCloseModal, modalRef);
+  useOutsideClickHandler(isModalOpen, onClose, modalRef);
 
   useEffect(() => {
     if (backdropRef.current) {
-      backdropRef.current.style.opacity = showModal ? '1' : '0';
-      backdropRef.current.style.pointerEvents = showModal ? 'auto' : 'none';
+      backdropRef.current.style.opacity = isModalOpen ? '1' : '0';
+      backdropRef.current.style.pointerEvents = isModalOpen ? 'auto' : 'none';
     }
-  }, [showModal]);
+  }, [isModalOpen]);
 
   return (
     <div
       ref={backdropRef}
       className={`fixed inset-0 z-40 flex items-center justify-center bg-black transition-opacity duration-500 ease-in-out ${
-        showModal ? 'bg-opacity-30' : 'bg-opacity-0 pointer-events-none'
+        isModalOpen ? 'bg-opacity-30' : 'bg-opacity-0 pointer-events-none'
       }`}
     >
       <div
         ref={modalRef}
         className={`fixed transform transition-all duration-500 ease-in-out ${
-          showModal ? 'scale-100 opacity-100' : 'scale-75 opacity-0'
+          isModalOpen ? 'scale-100 opacity-100' : 'scale-75 opacity-0'
         } top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50 rounded-[10px] shadow-2xl bg-white h-auto`}
         style={{ width }}
       >
@@ -51,12 +45,7 @@ const CModal: React.FC<CModalProps> = ({
           {title && (
             <header className="flex justify-between items-center text-2xl font-medium select-none">
               <p>{title}</p>
-              <Image
-                src={close}
-                alt="close"
-                onClick={handleCloseModal}
-                className="cursor-pointer"
-              />
+              <Image src={close} alt="close" onClick={onClose} className="cursor-pointer" />
             </header>
           )}
           {children}
--- src/containers/Test/index.tsx
+++ src/containers/Test/index.tsx
@@ -4,21 +4,21 @@ import React, { useState } from 'react';
 import CModal from '@/components/CModal';
 
 const Test = () => {
-  const [showModal, setShowModal] = useState(false);
-  const handleCloseModal = () => {
-    setShowModal(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const onClose = () => {
+    setIsModalOpen(false);
   };
 
   return (
     <div>
       <button
-        onClick={() => setShowModal(!showModal)}
+        onClick={() => setIsModalOpen(!isModalOpen)}
         className="h-10 w-64 rounded-lg text-mintGreen bg-darkGreen"
       >
         Click here
       </button>
 
-      <CModal title="Add payment" showModal={showModal} handleCloseModal={handleCloseModal}>
+      <CModal title="Add payment" isModalOpen={isModalOpen} onClose={onClose}>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas minus, rerum enim praesentium
         nostrum deserunt, amet officia quasi possimus earum dicta voluptate fugiat ad voluptatem
         impedit molestias non quae repellendus.

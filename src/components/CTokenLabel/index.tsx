41106386a748aa9f025e2cac82e30822e50235ca
dousdogukan <dousdogukan@gmail.com>
2024-07-04 15:17:13 +0330

    chore: add rounded props

--- src/components/CTokenLabel/index.tsx
+++ src/components/CTokenLabel/index.tsx
@@ -5,15 +5,18 @@ import tokensStyles from '@/components/CTokenLabel/tokensStyles';
 export interface CTokenLabelProps {
   symbol: string;
   logo: string;
+  rounded?: boolean;
 }
 
-const CTokenLabel = ({ symbol, logo }: CTokenLabelProps) => {
+const CTokenLabel = ({ symbol, logo, rounded }: CTokenLabelProps) => {
   const style = tokensStyles[symbol.toLowerCase()];
 
   return (
     <div>
       <div
-        className={`w-[90px] text-[14px] font-medium flex px-3 mobile:w-[80px] mobile:space-x-1 space-x-2 items-center py-[2px] rounded-[50px]`}
+        className={`w-[90px] font-medium flex px-3 mobile:w-[80px] mobile:space-x-1 space-x-1 items-center justify-center py-[2px] ${
+          rounded ? 'rounded-[50px] text-[14px] ' : 'rounded-[6px] text-base'
+        } `}
         style={{
           backgroundColor: style.bgColor,
           border: `2px solid ${style.borderColor}`,

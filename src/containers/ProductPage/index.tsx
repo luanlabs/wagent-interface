e471df91fc05e7c17c8782704aed846fa7cc191f
dousdogukan <dousdogukan@gmail.com>
2024-07-10 06:12:11 +0330

    chore: add modals to product page

--- src/containers/ProductPage/index.tsx
+++ src/containers/ProductPage/index.tsx
@@ -4,16 +4,41 @@ import React, { useState } from 'react';
 
 import CButton from '@/components/CButton';
 import CPageCard from '@/components/CPageCard';
-import { products } from '@/constants/productsList';
+import { initialProducts } from '@/constants/productsList';
 import CProductItemCard from '@/components/CProductItemCard';
 
 import AddProductModal from '../AddProductModal';
+import EditProductModal from '../EditProductModal';
 
 const ProductPage = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [AddProductIsOpen, setAddProductIsOpen] = useState(false);
+  const [EditProductIsOpen, setEditProductIsOpen] = useState(false);
+  const [products, setProducts] = useState<CProductItemCard[]>(initialProducts);
+  const [selectedProduct, setSelectedProduct] = useState<CProductItemCard>();
 
   const ModalOnClose = () => {
-    setIsOpen(false);
+    setAddProductIsOpen(false);
+    setEditProductIsOpen(false);
+  };
+
+  const handleAddProduct = (newProduct: CProductItemCard) => {
+    setProducts([...products, newProduct]);
+    setAddProductIsOpen(false);
+  };
+
+  const openEditModal = (product: CProductItemCard) => {
+    setSelectedProduct(product);
+
+    setEditProductIsOpen(true);
+  };
+
+  const handleSaveProduct = (updatedProduct: CProductItemCard) => {
+    setProducts(
+      products.map((product) => (product.id === updatedProduct.id ? updatedProduct : product)),
+    );
+
+    setEditProductIsOpen(false);
+    setSelectedProduct(undefined);
   };
 
   const pageTitle = (
@@ -23,7 +48,7 @@ const ProductPage = () => {
         variant="add"
         text="Add product"
         className="!w-[145px] text-base"
-        onClick={() => setIsOpen(true)}
+        onClick={() => setAddProductIsOpen(true)}
       />
     </div>
   );
@@ -35,7 +60,6 @@ const ProductPage = () => {
       className="h-[100%] relative overflow-hidden"
     >
       <h1 className="text-2xl py-4 font-medium">Products list</h1>
-
       <ul className="flex justify-between items-center rounded-[10px] bg-lightGray text-cadetBlue h-[42px] px-4 mb-4">
         <li className="text-black">Product name</li>
         <li className="mobile:hidden">Method</li>
@@ -43,23 +67,31 @@ const ProductPage = () => {
         <li className="w-1/5 mobile:hidden">Token</li>
         <li>Amount</li>
       </ul>
-
       <div className="space-y-3 pb-3 max-h-[calc(100vh-350px)] desktopMax:max-h-[calc(100vh-330px)] w-full overflow-hidden overflow-y-auto">
         {products.map((item) => (
-          <CProductItemCard
-            key={item.id}
-            title={item.title}
-            method={item.method}
-            id={item.id}
-            tokens={item.tokens}
-            amount={item.amount}
-            image={item.image}
-          />
+          <div key={item.id} onClick={() => openEditModal(item)}>
+            <CProductItemCard
+              title={item.title}
+              method={item.method}
+              id={item.id}
+              tokens={item.tokens}
+              amount={item.amount}
+              image={item.image}
+            />
+          </div>
         ))}
       </div>
-
-      <AddProductModal isOpen={isOpen} onClose={ModalOnClose} />
-
+      <AddProductModal
+        isOpen={AddProductIsOpen}
+        onClose={ModalOnClose}
+        onAddProduct={handleAddProduct}
+      />
+      <EditProductModal
+        isOpen={EditProductIsOpen}
+        onClose={ModalOnClose}
+        onSaveProduct={handleSaveProduct}
+        product={selectedProduct!}
+      />
       <div className="absolute bottom-0 left-0 w-full h-12 bg-gradient-to-t from-white to-transparent pointer-events-none"></div>
     </CPageCard>
   );
